f749356ea446036881a883f52dfae4245e63e337
machingol <machingol@gmail.com>
2016-02-28 16:48:39 +0100

    Variables.py

--- Variables.py
+++ Variables.py
@@ -29,3 +29,19 @@ print list;   #change the valor of position 2 from 0x16 to 25
 
 #unit(2)=25;
 print unit;   #ERROR,we cant change it :)
+
+
+#5, last one u can create a "table" , like a vector with valors, lets see..
+
+dict = {}
+dict['one'] = "This is one"
+dict[2]     = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+
+print dict['one']       # Prints value for 'one' key
+print dict[2]           # Prints value for 2 key
+print tinydict          # Prints complete dictionary
+print tinydict.keys()   # Prints all the keys
+print tinydict.values() # Prints all the values
